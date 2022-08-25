2c94aaebf231bf2ca5b2d244fe43552ae644535a
pyroYV <simonerizzo.pvt@gmail.com>
2022-08-25 17:05:02 +0200

    Feat: controller query check

--- backend/controller.php
+++ backend/controller.php
@@ -1,7 +1,22 @@
 <?php
     include __DIR__. '/db.php';
 
+    
+
+    if(isset($_GET['genre'])){
+        $genre = $_GET['genre'];
+        $filteredAlbums = [];
+        foreach($albums as $album){
+            if($album['genre'] == $genre){
+              $filteredAlbums[] = $album;
+            }
+        }
+    } else {
+        $filteredAlbums = $albums;
+    }
+
+
+
     header('Content-type: application/json');
 
-    echo json_encode(['success' => true,
-        'results' => ['data' => $albums]]);
+    echo json_encode($filteredAlbums);
